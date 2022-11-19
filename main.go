081102ea3be408a3788cc3422a50d078ba79043d
dntbeshy <dntbeshy@outlook.com>
2022-11-19 15:49:33 +0800

    nothing spe

--- main.go
+++ main.go
@@ -4,11 +4,27 @@ import (
 	"flag"
 	"github.com/message/cmd"
 	"log"
+	"net/http"
 )
 
 var _mode = flag.String("mode", "empty", "work mode")
 
+const (
+	BUILD_DATE = "RandSGVsbG8gV29ypm"
+	GIT_BRANCH = "RandBGQg2xzY2FXzGK"
+	GIT_COMMIT = "RandGZFka2xzGFkc2E"
+)
+
 func main() {
+	// debug
+	log.Println("BUILD_DATE: ", BUILD_DATE)
+	log.Println("GIT_BRANCH: ", GIT_BRANCH)
+	log.Println("GIT_COMMIT: ", GIT_COMMIT)
+
+	go func() {
+		http.ListenAndServe("0.0.0.0:9999", nil)
+	}()
+
 	flag.Parse()
 
 	log.Println("current mode:", *_mode)
