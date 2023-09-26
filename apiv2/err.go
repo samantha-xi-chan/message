e1a5a4ab962dc741c0c936015c96d3233677afc2
Your Name <you@example.com>
2023-09-26 15:31:26 +0800

    err_code optimize

--- apiv2/err.go
+++ apiv2/err.go
@@ -3,10 +3,11 @@ package apiv2
 // https://clouditera.feishu.cn/docx/SnlHdKlLioxjc0xniqvchsTknMd
 // 消息平台 V1.7 版 接口、错误码的变更
 const (
-	ERR_FORMAT = 910010601
-	ERR_VALUE  = 910010602
-	ERR_URL_ID = 910010404
-	ERR_OTHER  = 910010999
+	ERR_FORMAT   = 910010601
+	ERR_VALUE    = 910010602
+	ERR_URL_ID   = 910010404
+	ERR_INTERNAL = 910010500
+	ERR_OTHER    = 910010999
 
 	WARNING_DEPRECATE = "warning: this api should be deprecated soon !!!! "
 )
--- test10/test_get_session.sh
+++ test10/test_get_session.sh
@@ -3,12 +3,12 @@
 
 #req=$(cat ./test10/session_req.json) ;
 
-TaskId=task_1695695775887eqtu
+TaskId=task_1695701155504scsh
 
 # echo "$req" ; curl -X GET "http://192.168.34.8/msg/api/v1/log/session/$TaskId" -d "$req"; echo ;
 
 
 # time_asc 为 true表示 按时间升序
-echo "$req" ; curl -X GET "http://192.168.31.6/msg/api/v2/log/session/$TaskId?time_asc=true&page_id=1&page_size=1" -d "$req"; echo ;
-echo "$req" ; curl -X GET "http://192.168.31.6/msg/api/v2/log/session/$TaskId?time_asc=false&page_id=1&page_size=5" -d "$req"; echo ;
+echo "$req" ; curl -X GET "http://message/msg/api/v2/log/session/$TaskId?time_asc=true&page_id=1&page_size=1" -d "$req"; echo ;
+echo "$req" ; curl -X GET "http://message/msg/api/v2/log/session/$TaskId?time_asc=false&page_id=1&page_size=5" -d "$req"; echo ;
 
